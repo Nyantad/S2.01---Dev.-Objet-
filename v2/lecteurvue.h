e46a00838fdc87c0882a02b022c8f9ebbcb5ae4a
Mattis Pingard <mattis.pingard@gmail.com>
2023-05-03 16:05:47 +0200

    Création du fichier de la v2 avec les fichiers nécessaires

---
+++ v2/lecteurvue.h
@@ -0,0 +1,35 @@
+#ifndef LECTEURVUE_H
+#define LECTEURVUE_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class lecteurvue; }
+QT_END_NAMESPACE
+
+class lecteurvue : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    lecteurvue(QWidget *parent = nullptr);
+    ~lecteurvue();
+
+// déclaration des slots public
+public slots:
+    void lancer();
+    void arreter();
+    void precedent();
+    void suivant();
+    void modeManuel();
+    void modeAuto();
+    void quitter();
+    void charger();
+    void enlever();
+    void vitesse();
+    void aPropos();
+
+private:
+    Ui::lecteurvue *ui;
+};
+#endif // LECTEURVUE_H
