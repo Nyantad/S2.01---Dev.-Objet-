d12af734d6482c986cac5eb5f640d0bd8cc5acbc
Aurel <sucaurel@gmail.com>
2023-06-13 18:29:14 +0200

    ajout des commentaires dans lecteurvue.h dans la V0

--- v1_interface/lecteurvue.h
+++ v1_interface/lecteurvue.h
@@ -12,24 +12,24 @@ class lecteurvue : public QMainWindow
     Q_OBJECT
 
 public:
-    lecteurvue(QWidget *parent = nullptr);
-    ~lecteurvue();
+    lecteurvue(QWidget *parent = nullptr); // Constructeur pour la classe lecteurvue. Il accepte un QWidget parent optionnel
+    ~lecteurvue(); // Destructeur pour la classe lecteurvue
 
 // déclaration des slots public
 public slots:
-    void lancer();
-    void arreter();
-    void precedent();
-    void suivant();
-    void modeManuel();
-    void modeAuto();
-    void quitter();
-    void charger();
-    void enlever();
-    void vitesse();
-    void aPropos();
+    void lancer(); // Slot pour démarrer une certaine fonctionnalité
+    void arreter(); // Slot pour arrêter une certaine fonctionnalité
+    void precedent(); // Slot pour passer à la fonctionnalité précédente
+    void suivant(); // Slot pour passer à la fonctionnalité suivante
+    void modeManuel(); // Slot pour changer en mode manuel
+    void modeAuto(); // Slot pour changer en mode automatique
+    void quitter(); // Slot pour quitter l'application
+    void charger(); // Slot pour charger une nouvelle fonctionnalité ou un nouvel élément
+    void enlever(); // Slot pour enlever une fonctionnalité ou un élément
+    void vitesse(); // Slot pour changer la vitesse d'une certaine fonctionnalité
+    void aPropos(); // Slot pour afficher des informations sur l'application
 
 private:
-    Ui::lecteurvue *ui;
+    Ui::lecteurvue *ui; // Déclare un pointeur vers une instance de l'interface utilisateur pour la classe lecteurvue
 };
 #endif // LECTEURVUE_H
